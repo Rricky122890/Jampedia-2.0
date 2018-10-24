9668fd44ca6bb8dd173c82c4ef1c987c45eb53c3
shemarjoseph <shemarjoseph@yahoo.com>
2018-10-24 15:35:20 -0400

    Finish user sign up (for real this time)

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_160553) do
+ActiveRecord::Schema.define(version: 2018_10_24_192553) do
 
   create_table "adds", force: :cascade do |t|
     t.integer "playlist_id"
     t.integer "song_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "blurbs"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -37,6 +52,7 @@ ActiveRecord::Schema.define(version: 2018_10_24_160553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "username"
   end
 
 end
