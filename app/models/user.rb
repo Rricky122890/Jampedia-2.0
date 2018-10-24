0a4c6746b2d62d1cf99dea6eb14884ae78fb47b2
shemarjoseph <shemarjoseph@yahoo.com>
2018-10-24 15:15:21 -0400

    Finish user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-
   has_many :playlists
   has_many :adds, through: :playlists
 
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
