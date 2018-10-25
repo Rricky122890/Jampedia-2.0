262bbce5d6b8e99008d352e3a3062b941366f306
Carolyn Rosemary Winifred Cole <carolynrosemarywinifredcole@Carolyns-MacBook-Air.local>
2018-10-25 11:38:40 -0400

    bs time

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_10_24_154701) do
+ActiveRecord::Schema.define(version: 2018_10_24_192553) do
 
   create_table "adds", force: :cascade do |t|
     t.integer "playlist_id"
